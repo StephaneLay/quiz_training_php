4e57d3aabd600d28849dbd01cff82ca864428fe1
StephaneLay <stephane.lay@hotmail.fr>
2025-05-13 16:37:02 +0200

    new setup

--- quizzpage.php
+++ quizzpage.php
@@ -1,3 +1,8 @@
 <?php
+require "contents/questions.php";
 echo "<h1>prout : ".$_GET['category']."</h1>";
+echo $questionnaires[$_GET['category']];
 
+// IL FAUT :
+// CREER LE DISPLAY DE QUESTIONS , LE COMPTEUR DE POINTS , LE QUESITON TRACKER ETC DANS CE FICHIER !
+//PUIS CODER LES CONDITIONS DE WIN ET DE LOOSE AUSSI ET VERIFIER SI CA FONCTIONNE.
